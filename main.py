9923bdcf80636d645b195f3d3a185607adb32c75
Dimple Singh Raghav <142376329+dimplesingh23@users.noreply.github.com>
2025-10-08 17:31:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-from fastapi import FastAPI # type: ignore
-from dotenv import load_dotenv # type: ignore
+from fastapi import FastAPI 
+from dotenv import load_dotenv 
 from models import ChatRequest
 from chat_engine import get_response
 from crisis import contains_crisis_keywords, SAFETY_MESSAGE
 from logger import log_chat
+
