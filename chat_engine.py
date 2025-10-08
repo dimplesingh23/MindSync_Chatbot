f9c187c4e6ddd3b671462419015053dd4db46772
Dimple Singh Raghav <142376329+dimplesingh23@users.noreply.github.com>
2025-10-08 17:29:09 +0530

    Update chat_engine.py

--- chat_engine.py
+++ chat_engine.py
@@ -1,8 +1,8 @@
 import os
-from dostenv import load_dotenv # type: ignore
-from langchain_openai import OpenAI # type: ignore
-from langchain.chains import ConversationChain # type: ignore
-from langchain.memory import ConversationBufferMemory # type: ignore
+from dotenv import load_dotenv 
+from langchain_openai import OpenAI 
+from langchain.chains import ConversationChain 
+from langchain.memory import ConversationBufferMemory
 
 #load .env
 load_dotenv()
@@ -16,10 +16,11 @@ llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.7)
 #store per-user memory sessions
 session_memory_map = {}
 
+
 def get_response(session_id: str, user_query: str) -> str:
     if session_id not in session_memory_map:
         memory = ConversationBufferMemory()
-        session_memory_map[session_id] = ConversationChain(llm = llm, memory=memory, verbose= True)
+        session_memory_map[session_id] = ConversationChain(llm=llm, memory=memory, verbose=True)
 
     conversation = session_memory_map[session_id]
     return conversation.predict(input=user_query)
